e7f3c791923980d6fed9f050e6bd1365d9d35917
Peterturn <55192090+Peterturn@users.noreply.github.com>
2021-07-28 07:23:08 +0200

    switched class comp over to a stateless functional comp

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 
-class Like extends Component {
-    render() {
 
-        let classes = "fas fa-heart"; //empty
-        if (!this.props.like) {
-            classes = "far fa-heart";
-        }
-        return (
-            <i
-                onClick={this.props.onClick}
-                style={{ cursor: "pointer" }}
-                className={classes}
-                aria-hidden="true"
-            />
 
-        )
+const Like = (props) => {
+    let classes = "fas fa-heart"; //empty
+    if (!props.like) {
+        classes = "far fa-heart";
     }
+    return (
+        <i
+            onClick={props.onClick}
+            style={{ cursor: "pointer" }}
+            className={classes}
+            aria-hidden="true"
+        />
+
+    )
 }
 
 export default Like;
